38e40d03de802adef43801cfd0a35119929b6d90
sorayarezaie <banorezaie2004@gmail.com>
2025-04-13 19:37:10 +0430

    new

--- new html/app.js
+++ new html/app.js
@@ -1,26 +1,32 @@
-let b = 12;
-let a = 20;
-let result = b-a;
+//let b = 12;
+//let a = 20;
+//let result = b-a;
 
-let s = 344;
-let r = 'soraya';
-let numberOne = true;
+//let s = 344;
+//let r = 'soraya';
+//let numberOne = true;
 
-let persone = {'name': 'Soraya' , 'fathername' : 'asadullah', 'age': 20};
+//let persone = {'name': 'Soraya' , 'fathername' : 'asadullah', 'age': 20};
 // dot notation 
 //persone.name;
 
 //bracket notation
 //persone['name']
 
-let firstPro = 300;
-let secondPro = 280;
-let sam = firstPro + secondPro;
+//let firstPro = 300;
+//let secondPro = 280;
+//let sam = firstPro + secondPro;
 
 
-let computer = ['dill', 26000, 'black', '64GB'];
-let mCompany = {'name': 'mycrosoft', 'products': 'phone' , 'products': 'laptab'};
+//let computer = ['dill', 26000, 'black', '64GB'];
+//let mCompany = {'name': 'mycrosoft', 'products': 'phone' , 'products': 'laptab'};
 
-function greeting (){
-    console.log("hi dear Ashraf Omid");
+//function greeting (){
+  //  console.log("hi dear Ashraf Omid");
+//}
+//greeting();
+
+
+function hello(){
+    console.log("سلام علی صبح بخیر");
 }
