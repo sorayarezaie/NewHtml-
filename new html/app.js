15544e19808fd4a6b25afa8a16caa8f9f34c734b
sorayarezaie <banorezaie2004@gmail.com>
2025-04-22 16:40:23 +0430

    changes

--- new html/app.js
+++ new html/app.js
@@ -57,14 +57,14 @@
 //let product = 1000;
 //let result = product < 3 ?"this is not for usege": product < 5 ? "this is for usege": "this is well"
 
-let school = board ;
-if (school == board){
-  console.log("this is a board");
-}
-else if (school == marker){
-  console.log("this is a marker");
-}
-else {
-  console.log("I dont you");
-}
+//let school = board ;
+//if (school == board){
+  //console.log("this is a board");
+//}
+//else if (school == marker){
+ // //console.log("this is a marker");
+//}
+//else {
+  //console.log("I dont you");
+//}
 
